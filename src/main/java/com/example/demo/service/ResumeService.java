e6198514ece9a20ce8a498448386faf7ac593fc5
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-07-11 14:32:13 +0600

    Made bonus

--- src/main/java/com/example/demo/service/ResumeService.java
+++ src/main/java/com/example/demo/service/ResumeService.java
@@ -7,13 +7,38 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Data
 public class ResumeService {
     @Autowired
     private final ResumeDao resumeDao;
+
+    //BONUS
+    public Optional<Resume> getResumeById(int resumeId) {
+        // TODO: Реализовать получение резюме по идентификатору
+        return Optional.empty();
+    }
+
+
+
+    public Resume createResume(Resume resume) {
+        // TODO: Реализовать создание нового резюме
+        return null;
+    }
+
+    public Resume updateResume(Resume resume) {
+        // TODO: Реализовать обновление информации в резюме
+        return null;
+    }
+
+    public void deleteResume(int resumeId) {
+        // TODO: Реализовать удаление резюме по идентификатору
+    }
+    //Bonus окончен
 
     public ResumeService(ResumeDao resumeDao) {
         this.resumeDao = resumeDao;
