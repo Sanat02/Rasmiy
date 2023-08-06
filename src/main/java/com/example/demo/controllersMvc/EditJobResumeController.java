6c7fcc939ee2dbaec008a237f0b34169496e3ccf
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-08-06 15:44:40 +0600

    Modified database and added more values

--- src/main/java/com/example/demo/controllersMvc/EditJobResumeController.java
+++ src/main/java/com/example/demo/controllersMvc/EditJobResumeController.java
@@ -1,58 +1,56 @@
-package com.example.demo.controllersMvc;
-
-import com.example.demo.dto.JobResumeDto;
-import com.example.demo.service.CategoryService;
-import com.example.demo.service.JobResumeService;
-import com.example.demo.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("/jobresumes")
-public class EditJobResumeController {
-    private final UserService userService;
-    private final JobResumeService jobResumeService;
-
-
-    @GetMapping("/add/{userId}")
-    public String editJobResume(Model model, @PathVariable int userId) {
-        model.addAttribute("id", userId);
-        return "editVacancy";
-    }
-
-    @PostMapping("add/{userId}")
-    @ResponseStatus(HttpStatus.SEE_OTHER)
-    public String addJobResume(
-            @RequestParam(name = "resume_name") String vacancyName,
-            @RequestParam(name = "category") String category,
-            @RequestParam(name = "expected_salary") int expectedSalary,
-            @RequestParam(name = "vacancy-description") String description,
-            @RequestParam(name = "from_year") int startDate,
-            @RequestParam(name = "till_year") int endDate,
-            Authentication auth,
-            @PathVariable int userId
-    ) {
-        JobResumeDto jobResumeDto = JobResumeDto.builder()
-                .jobTitle(vacancyName)
-                .user(userService.mapToUserDto(userService.getUserById(userId).get()))
-                .jobDescription(description)
-                .salary(expectedSalary)
-                .experience(startDate)
-                .build();
-
-        int jobResumeId = jobResumeService.saveJobResume(jobResumeDto);
-        return "redirect:/jobresumes/" + jobResumeId;
-    }
-
-    @GetMapping("/{jobResumeId}")
-    public String getJobResume(Model model, @PathVariable int jobResumeId) {
-        JobResumeDto jobResumeDto = jobResumeService.mapToJobResumeDto(jobResumeService.getJobResumeById(jobResumeId).get());
-        model.addAttribute("jobresume", jobResumeDto);
-        return "seeVacancy";
-    }
-}
+//package com.example.demo.controllersMvc;
+//
+//import com.example.demo.dto.JobResumeDto;
+//import com.example.demo.service.CategoryService;
+//import com.example.demo.service.JobResumeService;
+//import com.example.demo.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping("/jobresumes")
+//public class EditJobResumeController {
+//    private final UserService userService;
+//    private final JobResumeService jobResumeService;
+//    @GetMapping("/add/{userId}")
+//    public String editJobResume(Model model, @PathVariable int userId) {
+//        model.addAttribute("id", userId);
+//        return "editVacancy";
+//    }
+//
+//    @PostMapping("add/{userId}")
+//    @ResponseStatus(HttpStatus.SEE_OTHER)
+//    public String addJobResume(
+//            @RequestParam(name = "resume_name") String vacancyName,
+//            @RequestParam(name = "category") String category,
+//            @RequestParam(name = "expected_salary") int expectedSalary,
+//            @RequestParam(name = "vacancy-description") String description,
+//            @RequestParam(name = "from_year") int startDate,
+//            @RequestParam(name = "till_year") int endDate,
+//            Authentication auth,
+//            @PathVariable int userId
+//    ) {
+//        JobResumeDto jobResumeDto = JobResumeDto.builder()
+//                .jobTitle(vacancyName)
+//                .user(userService.mapToUserDto(userService.getUserById(userId).get()))
+//                .jobDescription(description)
+//                .salary(expectedSalary)
+//                .experience(startDate)
+//                .build();
+//
+//        int jobResumeId = jobResumeService.saveJobResume(jobResumeDto);
+//        return "redirect:/jobresumes/" + jobResumeId;
+//    }
+//
+//    @GetMapping("/{jobResumeId}")
+//    public String getJobResume(Model model, @PathVariable int jobResumeId) {
+//        JobResumeDto jobResumeDto = jobResumeService.mapToJobResumeDto(jobResumeService.getJobResumeById(jobResumeId).get());
+//        model.addAttribute("jobresume", jobResumeDto);
+//        return "seeVacancy";
+//    }
+//}
--- src/main/java/com/example/demo/controllersMvc/ProfileController.java
+++ src/main/java/com/example/demo/controllersMvc/ProfileController.java
@@ -1,30 +1,30 @@
-package com.example.demo.controllersMvc;
-
-import com.example.demo.dto.UserDto;
-import com.example.demo.service.JobResumeService;
-import com.example.demo.service.ResumeService;
-import com.example.demo.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("/profile")
-public class ProfileController {
-    private final UserService userService;
-    private final ResumeService resumeService;
-    private final JobResumeService jobResumeService;
-
-    @GetMapping("/{userId}")
-    public String getMovie(@PathVariable int userId, Model model) {
-        UserDto userDto = userService.mapToUserDto(userService.getUserById(userId).get());
-        userDto.setResumes(resumeService.getResumesByUserId(userId));
-        userDto.setJobResumes(jobResumeService.getJobResumesByUserId(userId));
-        model.addAttribute("account", userDto);
-        return "profile";
-    }
-}
+//package com.example.demo.controllersMvc;
+//
+//import com.example.demo.dto.UserDto;
+//import com.example.demo.service.JobResumeService;
+//import com.example.demo.service.ResumeService;
+//import com.example.demo.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping("/profile")
+//public class ProfileController {
+//    private final UserService userService;
+//    private final ResumeService resumeService;
+//    private final JobResumeService jobResumeService;
+//
+//    @GetMapping("/{userId}")
+//    public String getMovie(@PathVariable int userId, Model model) {
+//        UserDto userDto = userService.mapToUserDto(userService.getUserById(userId).get());
+//        userDto.setResumes(resumeService.getResumesByUserId(userId));
+//        userDto.setJobResumes(jobResumeService.getJobResumesByUserId(userId));
+//        model.addAttribute("account", userDto);
+//        return "profile";
+//    }
+//}
