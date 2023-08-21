a38e798bc0026256c29f5497732f862ff9ae97e9
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-08-21 21:58:56 +0600

    Formatted the html code and added local storage

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,22 @@
+function saveUser(user){
+    let userAsJson=JSON.stringify(user)
+    localStorage.setItem('user',userAsJson)
+}
+
+function readUser(){
+    let userAsJson=localStorage.getItem('user')
+    let user=JSON.parse(userAsJson)
+    return user
+}
+
+function makeHeaders(){
+    let headers=new Headers()
+    let user=readUser()
+    headers.set('Content-Type','application/json')
+    if(user){
+        headers.set('Authorization','Basic'+btoa(user.email+":"+user.password))
+
+    }
+    return headers
+}
+
